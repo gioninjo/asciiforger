c0ffcd2d0a7d9aed7d20871b17a0df4f8c687003
gioninjo <80896175+gioninjo@users.noreply.github.com>
2024-05-11 13:38:06 +0200

    tests for video decoding now working

--- tests/test.rs
+++ tests/test.rs
@@ -35,7 +35,7 @@ fn video_decoding_test() {
     ffmpeg::init().unwrap();
 
     const TEST_STRING_PATH: &str = "tests/res/videotest";
-    const TEST_VIDEO_PATH: &str = "tests/res/video.avi";
+    const TEST_VIDEO_PATH: &str = "tests/res/video_test.mp4";
 
     let mut scale = *b"@%#*+=-:. ";
     scale.reverse();
@@ -43,26 +43,23 @@ fn video_decoding_test() {
         .unwrap_or(String::new())
         .chars()
         .collect();
-    println!("reading test file...");
     let test_string = fs::read_to_string(&String::from(TEST_STRING_PATH)).unwrap();
-    println!("done :)");
 
     let (img_vec, _time_interval) = read_video_and_decode(&String::from(TEST_VIDEO_PATH)).unwrap();
     let mut ascii_vec: Vec<String> = Vec::new();
 
     for image in img_vec {
-        let frame: String = asciify(128, 64, &img_scale, &image).unwrap();
+        let frame: String = match asciify(128, 64, &img_scale, &image) {
+            Ok(value) => value,
+            Err(_) => panic!("hey")
+        };
         ascii_vec.push(frame);
     }
 
     let mut video_string = String::new();
     for ascii_frame in ascii_vec {
         video_string.push_str(&ascii_frame);
-        video_string.push_str(&format!("{}[2J", 27 as char));
+        video_string.push_str(&format!("{}[2J ", 27 as char));
     }
-    println!("video_string {}", video_string.len());
-    println!("test_string {}", test_string.len());
-
     assert_eq!(video_string.len(), test_string.len());
-    println!("o");
 }
